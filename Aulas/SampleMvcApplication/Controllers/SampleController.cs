909069bf0e9cce1d1a3f7efbc795272aecbd4abb
lfalcao28 <lfalcao28@gmail.com>
2014-03-26 20:29:36 +0000

    Aula 7 - First MVC Application

---
+++ Aulas/SampleMvcApplication/Controllers/SampleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SampleMvcApplication.Controllers
+{
+    public class FooX
+    {
+
+        public FooX()
+        {
+            x = 22;
+        }
+
+        public FooX(String f)
+        {
+            foo = f;
+        }
+        public String foo { get; set; }
+        public int x { get; set; }
+    }
+
+    public class SampleController : Controller {
+
+        public String SayHello(FooX fx)
+        {
+            Response.ContentType = "image/gif";
+            return "Hello SLB " + fx.foo + " " + fx.x;
+        }
+        
+    }
+}
